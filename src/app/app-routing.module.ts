283018bd830c46c69a30a4c0e21087814b0e981f
alaa shaher <alaashaher12@gmail.com>
2019-09-16 22:30:03 +0200

    header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { RegisterComponent } from './components/register/register.component';
 const routes: Routes = [
-
+  { path: 'TodoListComponent', component: TodoListComponent },
+  { path: 'RegisterComponent', component: RegisterComponent }
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-todo-item-list';
+  constructor(private router: Router) { }
+  gotoHome() {
+    this.router.navigate(['TodoListComponent']);
+    console.log('todo');
+  }
+  gotoRedgester() {
+    this.router.navigate(['RegisterComponent']);
+  }
 }
